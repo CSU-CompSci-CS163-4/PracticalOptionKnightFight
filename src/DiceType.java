4c16bffb0bf5d7ec48158561e0314221e8d9bd8c
Logan-Seabolt <54724613+Logan-Seabolt@users.noreply.github.com>
2025-04-09 16:36:49 -0600

    Update DiceType.java

--- src/DiceType.java
+++ src/DiceType.java
@@ -1,3 +1,18 @@
 public enum DiceType {
-    D4, D6, D8, D10, D12, D20
+    D4(4), D6(6), D8(8), D10(10), D12(12), D20(20);
+
+	private final int size;
+	public static Random random_machine;
+
+	private Dice(int size) {
+		this.size = size;
+	}
+	/***
+	 * Roll
+	 * 
+	 * @return int representing a random roll between 1 to size
+	 */
+	public int Roll() {
+		return random_machine.nextInt(this.size) + 1;
+	}
 }
